185bedec6036e31b767a3b6e859327686ef49101
Spiritbr8ker <stephen.oveson@gmail.com>
2019-08-20 17:36:01 -0600

    database changes for jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR (50) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT FALSE,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
