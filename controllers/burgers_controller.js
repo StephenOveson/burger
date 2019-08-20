907190b01f17c69e9df2b6396b774c8dce6b81d5
Spiritbr8ker <stephen.oveson@gmail.com>
2019-08-19 20:15:31 -0600

    Working app now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 
-let burger = require('../models/burger.js')
+let burger = require('./../models/burger')
 
 router.get('/', (req, res) => {
     burger.select(function(results){
--- server.js
+++ server.js
@@ -1,27 +1,21 @@
-let express = require("express");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-let PORT = process.env.PORT || 8080;
+const handlebars = require('express-handlebars');
 
-let app = express();
+app.use('/public', express.static('public'))
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// Set Handlebars.
-let exphbs = require("express-handlebars");
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+let router = require('./controllers/burgers_controller')
 
-// Import routes and give the server access to them.
-let routes = require("./controllers/burgers_controller.js");
+app.use(router);
 
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+app.listen(PORT, ()=> {
+    console.log("listening on " + PORT)
+})
