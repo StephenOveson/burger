f2759d302ea04c998de30c23d6fedbb85be4dc49
Spiritbr8ker <stephen.oveson@gmail.com>
2019-08-20 17:26:28 -0600

    jawsdb added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 let mysql = require('mysql')
+let connection;
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: 8889,
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+}
 
 connection.connect(function(err){
     if (err){
